d242bf2198fff12b22f2bc676d52aa85c146ed39
Tayte Boss <boss.tayte@gmail.com>
2023-03-01 15:23:32 +1100

    [feat] Featured Projects - Adding a marquee ticker on longer client names

--- components/blocks/FeaturedProjects/FeaturedProjectCard.tsx
+++ components/blocks/FeaturedProjects/FeaturedProjectCard.tsx
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 import { FeaturedProjectsType } from '../../../shared/types/types';
 import Link from 'next/link';
 import pxToRem from '../../../utils/pxToRem';
-import { useContext } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { PreviewContext } from '../../../shared/context/previewContext';
+import Marquee from 'react-fast-marquee';
 
 const FeaturedProjectCardWrapper = styled.a`
 	text-decoration: none;
 	display: flex;
-	margin-bottom: ${pxToRem(4)};
+	height: 60px;
 
 	&:hover {
 		opacity: 1 !important;
@@ -28,19 +29,22 @@ const ClientWrapper = styled.div`
 	overflow: hidden;
 `;
 
+const ClientInner = styled.div``;
+
 const Client = styled.div`
 	padding: 0 ${pxToRem(16)};
 	overflow: hidden;
-	display: inline;
+	display: inline-block;
 	transition: all var(--transition-speed-default) var(--transition-ease);
 	white-space: nowrap;
+	width: 100%;
 `;
 
 const TitleWrapper = styled.div``;
 
 const Title = styled.div`
 	padding: 0 ${pxToRem(16)};
-	display: inline;
+	display: inline-block;
 	transition: all var(--transition-speed-default) var(--transition-ease);
 `;
 
@@ -53,24 +57,100 @@ const FeaturedProjectCard = (props: Props) => {
 		data
 	} = props;
 
-	const { previewData, setPreviewData } = useContext(PreviewContext);
+	const [useMarquee, setUseMarquee] = useState<boolean>(false);
+	const [isHovered, setIsHovered] = useState<boolean>(false);
+	const [outerHover, setOuterHover] = useState<boolean>(false);
+
+	const { setPreviewData } = useContext(PreviewContext);
+
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	const handleOuterMouseOver = () => {
+		setPreviewData(data?.media);
+		setOuterHover(true);
+	};
+
+	const handleOuterMouseOut = () => {
+		setPreviewData(false);
+		setOuterHover(false);
+	};
+
+	useEffect(() => {
+		const textRef = containerRef?.current;
+
+		if (!textRef) return;
+
+		if (textRef?.scrollWidth > textRef?.offsetWidth) {
+			setUseMarquee(true);
+		}
+	}, []);
 
 	return (
 		<Link href={data?.vimeoLink} passHref>
 			<FeaturedProjectCardWrapper
 				className="featured-project-card"
 				target="_blank"
-				onMouseOver={() => setPreviewData(data?.media)}
-				onMouseOut={() => setPreviewData(false)}
+				onMouseOver={() => handleOuterMouseOver()}
+				onMouseOut={() => handleOuterMouseOut()}
 			>
-				<ClientWrapper>
-					{data?.client && (
-						<Client className="type-large featured-client">{data?.client}</Client>
-					)}
-				</ClientWrapper>
+				{useMarquee && (
+					<>
+						{outerHover && (
+							<ClientWrapper
+								onMouseOver={() => setIsHovered(true)}
+								onMouseOut={() => setIsHovered(false)}
+							>
+								<ClientInner className="featured-client">
+									{data?.client && (
+										<Marquee
+											gradient={false}
+											speed={75}
+											play={isHovered}
+										>
+											<Client
+												className="type-large"
+												ref={containerRef}
+											>
+												{data?.client}
+											</Client>
+										</Marquee>
+									)}
+								</ClientInner>
+							</ClientWrapper>
+						)}
+						{!outerHover && (
+							<ClientWrapper>
+								{data?.client && (
+									<Client
+										className="type-large featured-client"
+										ref={containerRef}
+									>
+										{data?.client}
+									</Client>
+								)}
+							</ClientWrapper>
+						)}
+					</>
+				)}
+				{!useMarquee && (
+					<ClientWrapper>
+						{data?.client && (
+							<Client
+								className="type-large featured-client"
+								ref={containerRef}
+							>
+								{data?.client}
+							</Client>
+						)}
+					</ClientWrapper>
+				)}
 				<TitleWrapper>
 					{data?.title && (
-						<Title className="type-large featured-title">{data?.title}</Title>
+						<Title
+							className="type-large featured-title"
+						>
+							{data?.title}
+						</Title>
 					)}
 				</TitleWrapper>
 			</FeaturedProjectCardWrapper>
